351d93eafa5f62a7b4e98f5b5f48ffc67d99a84a
Rafael Alberto Martinez Mendez <rmartinez@capi.lat>
2022-10-26 04:41:00 -0500

    Group by dimension in folders, input destination folder and minor changes

--- getWallpapers.py
+++ getWallpapers.py
@@ -6,46 +6,65 @@ user32 = ctypes.windll.user32
 f_width = user32.GetSystemMetrics(0)
 f_height = user32.GetSystemMetrics(1)
 
-def copy_files(base_folder):
-  source_folder = os.path.expanduser('~') + '/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets/'
-  destination_folder = base_folder
+images_path = '/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets/'
+extension_convert = '.jpg'
 
-  shutil.copytree(source_folder, destination_folder)
-  return destination_folder
 
-def convert_to_images(base_folder):
-  for file in os.listdir(base_folder):
+def copy_files():
+    source_folder = os.path.expanduser('~') + images_path
+    destination_folder = destination
 
-   head, tail = os.path.splitext(file)
-   if not tail:
-       src = os.path.join(base_folder, file)
-       dst = os.path.join(base_folder, file + '.jpg')
-
-       if not os.path.exists(dst): # check if the file doesn't exist
-           os.rename(src, dst)
-
-# TODO: Segmentar en carpetas segun dimensiones de imagen
-def group_by_screensize(base_folder):
-  from PIL import Image
-  for file in os.listdir(base_folder):
-    im = Image.open(os.path.join(base_folder, file))
-    i_width, i_height = im.size
-    if i_width == f_width and i_height == f_height:
-      print(file)
-    elif i_width == f_height and i_height == f_width:
-      print(file)
+    if not os.path.isdir(destination):
+        shutil.copytree(source_folder, destination_folder)
+        return destination_folder
     else:
-      im.close()
-      os.remove(os.path.join(base_folder, file))
+        print("Folder already exists")
 
 
-# TODO: * Destination_folder debera ser input
+def convert_to_images():
+    for file in os.listdir(destination):
+
+        head, tail = os.path.splitext(file)
+        if not tail:
+            src = os.path.join(destination, file)
+            dst = os.path.join(destination, file + extension_convert)
+
+            if not os.path.isdir(dst):
+                os.rename(src, dst)
+
+
+def make_folders_by_size():
+    names = ['landscape', 'portrait']
+    mode = 0o777
+
+    for name in names:
+        path = os.path.join(destination, name)
+        os.mkdir(path, mode)
+
+
+def group_by_screensize():
+    from PIL import Image
+    for file in os.listdir(destination):
+        im = Image.open(os.path.join(destination, file))
+        i_width, i_height = im.size
+        im.close()
+        if i_width == f_width and i_height == f_height:
+            shutil.move(os.path.join(destination, file),
+                        os.path.join(destination, 'landscape'))
+        elif i_width == f_height and i_height == f_width:
+            shutil.move(os.path.join(destination, file),
+                        os.path.join(destination, 'portrait'))
+        else:
+            os.remove(os.path.join(destination, file))
+
+
 def main():
-  folder_name = r"C:/Users/bakur/OneDrive/Escritorio/Wallpapers/"
+    copy_files()
+    convert_to_images()
+    make_folders_by_size()
+    group_by_screensize()
 
-  copy_files(folder_name)
-  convert_to_images(folder_name)
-  group_by_screensize(folder_name)
 
-
+destination = raw_input("Enter the folder name to save images: ")
+destination = os.path.join(os.getcwd(), destination)
 main()
